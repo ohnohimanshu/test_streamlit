8f245c509808be444aa953821997313d6966c689
Himanshu Sharma <himanshuxdei@gmail.com>
2025-05-23 14:49:56 +0530

    Create wish.py

---
+++ wish.py
@@ -0,0 +1,14 @@
+def wish_internship(name):
+    """
+    Generate a personalized internship wish message for the given name
+    """
+    wish_message = f"""
+    Dear {name},
+    
+    Wishing you the very best of luck in your internship search! 🌟
+    May your skills shine bright and opportunities find their way to you.
+    Keep working hard and stay focused - your dream internship is just around the corner!
+    
+    Best wishes for your success! 💪
+    """
+    return wish_message
